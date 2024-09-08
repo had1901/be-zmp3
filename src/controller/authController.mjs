86641d483f16514403d42cb0916218b1a30a0d18
had191 <senna2k20@gmail.com>
2024-09-08 22:39:12 +0700

    Edit setCookie sameSite=none

--- src/controller/authController.mjs
+++ src/controller/authController.mjs
@@ -95,8 +95,8 @@ const authController = {
                 }
                 res.setHeader('Set-Cookie', 
                     [
-                        `token=${access_token}; HttpOnly; Secure; SameSite:None; Path=/`,
-                        `refreshToken=${refresh_token}; HttpOnly; Secure; SameSite:None; Path=/; Expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}`
+                        `token=${access_token}; HttpOnly; Secure; SameSite=None; Path=/`,
+                        `refreshToken=${refresh_token}; HttpOnly; Secure; SameSite=None; Path=/; Expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}`
                     ])
                 const isCheckPassword = await checkPassword(data.password, user.password)
                 console.log('isCheckPassword: ===', isCheckPassword)
